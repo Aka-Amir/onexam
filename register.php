d7ea9f151903e87de51b6b0f772e0bef470edab5
ArcherFs <khaliliamir565@gmail.com>
2020-03-17 22:34:48 +0330

    Blink

--- register.php
+++ register.php
@@ -13,12 +13,18 @@
 <div class="form">
     <div class="main">
         <form action="register" method="POST">
-            
+
             <label for="fullname">نام و نام خانوادگی :‌</label><br>
             <input type="text" name="fullname" required>
-
+            <br>
+            <label for="passwd">رمز عبور :‌ <i class="fas fa-eye" id="eye" role="button" title="نشان دادن"></i>  </label>
+            <br>
+            <input type="password" name="passwd">
+            <br>
             <input type="number" placeholder="<?php echo $x . '+' . $y; ?>" name="antispam" autocomplete="off" required> <br>
-            <button type="submit">ورود</button>
+            <button type="submit">
+                ثبت نام <i class="fa fa-user-plus"></i>
+            </button>
         </form>
     </div>
 
